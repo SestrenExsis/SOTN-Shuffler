46a9d571c8a7792da0e7d127ea93e0224d66d2bf
SestrenExsis <sestrenexsis@gmail.com>
2024-09-14 08:11:10 -0500

    Move to sandbox; Rename files

--- MakeLogicFile.py
+++ MakeLogicFile.py
@@ -8,14 +8,46 @@ import yaml
 
 if __name__ == '__main__':
     logic = {}
-    for file_name in (
-        'logic/default.yaml',
-        'logic/castle-entrance.yaml',
+    for room_name in (
+        'Bat Card Room',
+        'Bloody Zombie Hallway',
+        'Blue Door Hallway',
+        'Box Puzzle Room',
+        'Cannon Room',
+        'Cloth Cape Room',
+        'Corridor to Elevator',
+        'Elevator Shaft',
+        'Empty Zig Zag Room',
+        'Entryway',
+        'Exit to Holy Chapel',
+        'Exit to Marble Gallery',
+        'Fake Castle Entrance Room',
+        'Fake Marble Gallery Room',
+        'Fake Royal Chapel Room',
+        'Glass Vats',
+        'Heart Max-Up Room',
+        'Loading Room A',
+        'Loading Room B',
+        'Loading Room C',
+        'Red Skeleton Lift Room',
+        'Save Room A',
+        'Save Room B',
+        'Save Room C',
+        'Secret Life Max-Up Room',
+        'Short Zig Zag Room',
+        'Skill of Wolf Room',
+        'Slogra and Gaibon Boss Room',
+        'Sunglasses Room',
+        'Tall Spittlebone Room',
+        'Tall Zig Zag Room',
+        'Tetromino Room',
     ):
+        file_name = 'data/rooms/alchemy-laboratory/' + room_name + '.yaml'
         with open(file_name) as open_file:
             yaml_obj = yaml.safe_load(open_file)
-            logic.update(yaml_obj)
-    with open('logic/logic.json', 'w') as open_file:
+            logic[room_name] = {}
+            logic[room_name] = yaml_obj
+    with open('build/logic.json', 'w') as open_file:
         json_string = json.dumps(
             logic,
             indent='    ',
