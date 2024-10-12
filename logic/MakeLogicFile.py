01c2962323f568640f1b48c8ccf3626a1cb787c1
SestrenExsis <sestrenexsis@gmail.com>
2024-10-12 14:41:23 -0500

    Begin process of reorganizing logic file

--- logic/MakeLogicFile.py
+++ logic/MakeLogicFile.py
@@ -2,34 +2,80 @@ import json
 import os
 import yaml
 
-# Triggers in the logic file have the following restrictions:
-# - Requirements are interchangeable; any one of them can be used to satisfy the conditions for the trigger
+# Commands in the logic file have the following restrictions:
+# - Requirements are interchangeable; any one of them can be used to satisfy the conditions for the command
 # - Requirements may not alter state
 # - Outcomes are the only way to alter state
 
 if __name__ == '__main__':
     logic = {
-        'Rooms': {},
-        'Teleporters': {},
+        'State': {
+            'Character': 'Alucard',
+            'Location': 'Castle Entrance, Entryway',
+            'Section': 'Ground',
+            'Item - Alucard Sword': 1,
+            'Item - Alucard Shield': 1,
+            'Item - Dragon Helm': 1,
+            'Item - Alucard Mail': 1,
+            'Item - Twilight Cloak': 1,
+            'Item - Necklace of J': 1,
+            'Item - Neutron Bomb': 1,
+            'Item - Heart Refresh': 1,
+        },
+        'Goals': {
+            'Debug - Reach Loading Room A': {
+                'Location': 'Castle Entrance, Loading Room A',
+            },
+            'Debug - Reach Loading Room B': {
+                'Location': 'Castle Entrance, Loading Room B',
+            },
+            'Debug - Reach Loading Room C': {
+                'Location': 'Castle Entrance, Loading Room C',
+            },
+            'Debug - Reach Loading Room D': {
+                'Location': 'Castle Entrance, Loading Room D',
+            },
+        },
+        'Commands': {},
+        # 'Teleporters': {},
     }
-    for (stage_folder, stage_name) in (
-        ('castle-entrance', 'Castle Entrance'),
-        ('castle-entrance-revisited', 'Castle Entrance Revisited'),
-        ('alchemy-laboratory', 'Alchemy Laboratory'),
+    for stage_folder in (
+        'castle-entrance',
+        # ('castle-entrance-revisited', 'Castle Entrance Revisited'),
+        # ('alchemy-laboratory', 'Alchemy Laboratory'),
     ):
         folder_path = os.path.join('data', 'rooms', stage_folder)
         for file_name in os.listdir(folder_path):
             if file_name[-5:] != '.yaml':
                 continue
-            room_name = file_name[:-5]
             file_path = os.path.join(folder_path, file_name)
             with open(file_path) as open_file:
                 yaml_obj = yaml.safe_load(open_file)
-                logic['Rooms'][stage_name + ', ' + room_name] = yaml_obj
-    with open(os.path.join('data', 'Teleporters.yaml')) as open_file:
-        yaml_obj = yaml.safe_load(open_file)
-        logic['Teleporters'] = yaml_obj
-    with open(os.path.join('build', 'logic.json'), 'w') as open_file:
+                location_name = yaml_obj['Stage'] + ', ' + yaml_obj['Room']
+                logic['Commands'][location_name] = yaml_obj['Commands']
+                for (exit_key, node) in yaml_obj['Nodes'].items():
+                    # Exit - Lower-Right Passage:
+                    outcomes = {}
+                    # TODO(sestren): Match edge based on map position inside the same stage
+                    matching_location_name = 'Castle Entrance, Zombie Hallway'
+                    matching_section = 'Main'
+                    exit = {
+                        'Outcomes': {
+                            'Location': matching_location_name,
+                            'Section': matching_section,
+                        },
+                        'Requirements': {
+                            'Default': {
+                                'Location': location_name,
+                                'Section': node['Section']
+                            },
+                        },
+                    }
+                    logic['Commands'][location_name]['Exit - ' + exit_key] = exit
+    # with open(os.path.join('data', 'Teleporters.yaml')) as open_file:
+    #     yaml_obj = yaml.safe_load(open_file)
+    #     logic['Teleporters'] = yaml_obj
+    with open(os.path.join('build', 'logic', 'vanilla-logic.json'), 'w') as open_file:
         json_string = json.dumps(
             logic,
             indent='    ',
--- randomize.bat
+++ randomize.bat
@@ -1,3 +1,4 @@
-python MakeLogicFile.py
-python roomrando/shuffler.py > build/spoiler.out
-python roomrando/patcher.py
+python logic/MakeLogicFile.py
+python logic/TestLogic.py
+@REM python roomrando/shuffler.py > build/spoiler.out
+@REM python roomrando/patcher.py
