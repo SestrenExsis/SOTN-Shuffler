1cf57baeda0df8e4420ab61feeaecd846534476d
SestrenExsis <sestrenexsis@gmail.com>
2024-11-05 10:59:23 -0600

    Memoize solving to improve performance

--- roomrando/solver.py
+++ roomrando/solver.py
@@ -158,20 +158,26 @@ class Solver():
     
     def solve(self, reflexive_limit: int=3, max_layers: int=8):
         # TODO(sestren): Improve performance with memoization
+        memo = {}
         solution_found = False
         initial_game = Game(self.logic_core['State'], self.logic_core['Commands'], self.logic_core['Goals'])
         work__solver = collections.deque()
         work__solver.appendleft((0, initial_game))
         while len(work__solver) > 0 and not solution_found:
             (step__solver, game__solver) = work__solver.pop()
-            print(step__solver, game__solver.state['Location'])
             game__solver.layer = step__solver
             if game__solver.goal_achieved:
                 solution_found = True
                 self.results['Wins'].append((step__solver, game__solver))
                 break
+            (_, _, hashed_state__solver) = game__solver.get_key()
+            if hashed_state__solver in memo and memo[hashed_state__solver] <= step__solver:
+                print('seen', hashed_state__solver, 'with layer', memo[hashed_state__solver])
+                continue
+            memo[hashed_state__solver] = step__solver
             if step__solver >= max_layers:
                 continue
+            print(step__solver, game__solver.state['Location'])
             # Find all locations that are N-bonded with the current location (N = reflexive_limit)
             # Two locations are considered "N-bonded" if you can move from one to another via a series of N-reflexive commands
             # A command is considered "N-reflexive" if you can return to the same state as you had before executing it in at most N additional commands
@@ -259,17 +265,17 @@ if __name__ == '__main__':
             # 'Debug 6': {
             #     'Relic - Soul of Wolf': True,
             # },
-            'Debug 7': {
-                'Location': 'Marble Gallery, Long Hallway',
-                'Relic - Soul of Wolf': True,
-            },
+            # 'Debug 7': {
+            #     'Location': 'Marble Gallery, Long Hallway',
+            #     'Relic - Soul of Wolf': True,
+            # },
             # 'Debug 8': {
             #     'Location': 'Marble Gallery, Clock Room',
             #     'Relic - Soul of Wolf': True,
             # },
-            # 'Debug 9': {
-            #     'Location': 'Marble Gallery, Loading Room D',
-            # },
+            'Debug 9': {
+                'Location': 'Marble Gallery, Loading Room D',
+            },
             # 'Debug 99': {
             #     'Location': 'Colosseum, Entrance',
             #     'Relic - Form of Mist': True,
