b676dafd68d7648a29473c01d920f98398d43445
SestrenExsis <sestrenexsis@gmail.com>
2024-11-05 07:57:36 -0600

    Check for win state more often

--- roomrando/solver.py
+++ roomrando/solver.py
@@ -16,8 +16,11 @@ class Game:
         self.commands = commands
         self.goals = goals
         self.starting_state = copy.deepcopy(starting_state)
+        # Must add the following to self.clone()
         self.history = []
         self.stages_visited = set()
+        self.goal_achieved = False
+        self.layer = 0
         self.debug = False
     
     @property
@@ -30,7 +33,12 @@ class Game:
     
     def clone(self):
         result = Game(self.state, self.commands, self.goals)
+        result.layer = self.layer
         result.history = list(self.history)
+        self.stages_visited = set(self.stages_visited)
+        self.goal_achieved = self.goal_achieved
+        self.layer = self.layer
+        self.debug = self.debug
         return result
     
     def get_key(self) -> int:
@@ -74,7 +82,7 @@ class Game:
 
     def process_command(self, command_name: str):
         location = self.state['Location']
-        self.history.append((command_name, location))
+        self.history.append((self.layer, location, command_name))
         stage = location[:location.find(',')]
         self.stages_visited.add(stage)
         command_data = {}
@@ -92,6 +100,17 @@ class Game:
                 if self.debug:
                     print('  +', key, ': ', value)
                 self.state[key] += value
+        if not self.goal_achieved:
+            goal_achieved = False
+            for (goal_name, requirements) in self.goals.items():
+                for (key, expected_value) in requirements.items():
+                    if key not in self.state or self.state[key] != expected_value:
+                        break
+                else:
+                    goal_achieved = True
+                    break
+            if goal_achieved:
+                self.goal_achieved = True
 
     def get_valid_command_names(self) -> list:
         result = set()
@@ -140,48 +159,67 @@ class Solver2():
         self.logic_core = logic_core
         for (skill_key, skill_value) in skills.items():
             self.logic_core['State'][skill_key] = skill_value
-        self.logic_core['State']['Location'] = 'Marble Gallery, Beneath Dropoff'
-        self.logic_core['State']['Section'] = 'Main'
+        self.results = {
+            'Wins': [],
+            'Losses': [],
+        }
     
-    def solve(self, reflexive_limit: int=3):
-        # Find all locations that are N-bonded with the current location (N = reflexive_limit)
-        # Two locations are considered "N-bonded" if you can move from one to another via a series of N-reflexive commands
-        # A command is considered "N-reflexive" if you can return to the same state as you had before executing it in at most N additional commands
-        bonded_locations = {}
-        work__bonded = collections.deque()
-        work__bonded.appendleft((0, Game(self.logic_core['State'], self.logic_core['Commands'], self.logic_core['Goals'])))
-        while len(work__bonded) > 0:
-            (step__bonded, game__bonded) = work__bonded.pop()
-            # print('-', game__bonded.location)
-            if game__bonded.location in bonded_locations:
+    def solve(self, reflexive_limit: int=3, max_layers: int=8):
+        solution_found = False
+        initial_game = Game(self.logic_core['State'], self.logic_core['Commands'], self.logic_core['Goals'])
+        work__solver = collections.deque()
+        work__solver.appendleft((0, initial_game))
+        while len(work__solver) > 0 and not solution_found:
+            (step__solver, game__solver) = work__solver.pop()
+            game__solver.layer = step__solver
+            if game__solver.goal_achieved:
+                solution_found = True
+                self.results['Wins'].append((step__solver, game__solver))
+                break
+            if step__solver >= max_layers:
                 continue
-            bonded_locations[game__bonded.location] = set()
-            (_, _, hashed_state__bonded) = game__bonded.get_key()
-            # Find N-reflexive commands
-            reflexive_command_names = set()
-            work__reflexive = collections.deque()
-            for command in game__bonded.get_valid_command_names():
-                work__reflexive.appendleft((0, command, command, game__bonded.clone()))
-                bonded_locations[game__bonded.location].add(command)
-            while len(work__reflexive) > 0:
-                (step__reflexive, original_command__reflexive, current_command__reflexive, game__reflexive) = work__reflexive.pop()
-                # print(' ' * step__reflexive, step__reflexive, original_command__reflexive, current_command__reflexive)
-                game__reflexive.process_command(current_command__reflexive)
-                (_, _, hashed_state__reflexive) = game__reflexive.get_key()
-                if hashed_state__reflexive == hashed_state__bonded:
-                    reflexive_command_names.add(original_command__reflexive)
+            # Find all locations that are N-bonded with the current location (N = reflexive_limit)
+            # Two locations are considered "N-bonded" if you can move from one to another via a series of N-reflexive commands
+            # A command is considered "N-reflexive" if you can return to the same state as you had before executing it in at most N additional commands
+            bonded_locations = {}
+            work__bonded = collections.deque()
+            work__bonded.appendleft((0, game__solver))
+            while len(work__bonded) > 0 and not solution_found:
+                (step__bonded, game__bonded) = work__bonded.pop()
+                if game__bonded.goal_achieved:
+                    solution_found = True
+                    self.results['Wins'].append((step__solver + 1, game__bonded))
+                    break
+                if game__bonded.location in bonded_locations:
                     continue
-                if step__reflexive < reflexive_limit:
-                    for next_command__reflexive in game__reflexive.get_valid_command_names():
-                        work__reflexive.appendleft((step__reflexive + 1, original_command__reflexive, next_command__reflexive, game__reflexive.clone()))
-            for reflexive_command_name in reflexive_command_names:
-                next_game__bonded = game__bonded.clone()
-                next_game__bonded.process_command(reflexive_command_name)
-                work__bonded.append((step__bonded + 1, next_game__bonded))
-            bonded_locations[game__bonded.location] -= reflexive_command_names
-        print('3-bonded locations:')
-        for (bonded_location, valid_commands) in sorted(bonded_locations.items()):
-            print('-', bonded_location, ':', valid_commands)
+                bonded_locations[game__bonded.location] = [game__bonded, set()]
+                (_, _, hashed_state__bonded) = game__bonded.get_key()
+                # Find all N-reflexive commands at the current location
+                reflexive_command_names = set()
+                work__reflexive = collections.deque()
+                for command in game__bonded.get_valid_command_names():
+                    work__reflexive.appendleft((0, command, command, game__bonded.clone()))
+                    bonded_locations[game__bonded.location][1].add(command)
+                while len(work__reflexive) > 0:
+                    (step__reflexive, original_command__reflexive, current_command__reflexive, game__reflexive) = work__reflexive.pop()
+                    game__reflexive.process_command(current_command__reflexive)
+                    (_, _, hashed_state__reflexive) = game__reflexive.get_key()
+                    if hashed_state__reflexive == hashed_state__bonded:
+                        reflexive_command_names.add(original_command__reflexive)
+                        continue
+                    if step__reflexive < reflexive_limit:
+                        for next_command__reflexive in game__reflexive.get_valid_command_names():
+                            work__reflexive.appendleft((step__reflexive + 1, original_command__reflexive, next_command__reflexive, game__reflexive.clone()))
+                for reflexive_command_name in reflexive_command_names:
+                    next_game__bonded = game__bonded.clone()
+                    next_game__bonded.process_command(reflexive_command_name)
+                    work__bonded.append((step__bonded + 1, next_game__bonded))
+                bonded_locations[game__bonded.location][1] -= reflexive_command_names
+            for (bonded_location, (game, valid_commands)) in sorted(bonded_locations.items()):
+                for next_command__solver in valid_commands:
+                    next_game__solver = game.clone()
+                    next_game__solver.process_command(next_command__solver)
+                    work__solver.appendleft((step__solver + 1, next_game__solver))
 
 class Solver():
     def __init__(self, logic_core, skills):
@@ -256,27 +294,66 @@ if __name__ == '__main__':
         with open(os.path.join('build', 'sandbox', 'data-core.json'), 'w') as data_core_json:
             json.dump(data_core, data_core_json, indent='    ', sort_keys=True)
         logic_core = roomrando.LogicCore(data_core, {}).get_core()
+        logic_core['State']['Location'] = 'Castle Entrance, After Drawbridge'
+        logic_core['State']['Section'] = 'Ground'
+        # logic_core['State']['Location'] = 'Alchemy Laboratory, Slogra and Gaibon Boss Room'
+        # logic_core['State']['Section'] = 'Ground'
         logic_core['Goals'] = {
-            'Debug': {
+            # 'Debug 1': {
+            #     'Location': 'Castle Entrance, Cube of Zoe Room',
+            # },
+            # 'Debug 2': {
+            #     'Location': 'Alchemy Laboratory, Slogra and Gaibon Boss Room',
+            # },
+            # 'Debug 3': {
+            #     'Location': 'Alchemy Laboratory, Bloody Zombie Hallway',
+            # },
+            # 'Debug 4': {
+            #     'Location': 'Marble Gallery, Long Hallway',
+            # },
+            # 'Debug 5': {
+            #     'Location': 'Outer Wall, Exit to Marble Gallery',
+            # },
+            # 'Debug 6': {
+            #     'Relic - Soul of Wolf': True,
+            # },
+            'Debug 7': {
+                'Location': 'Marble Gallery, Clock Room',
+                'Relic - Soul of Wolf': True,
+            },
+            'Debug 8': {
                 'Location': 'Marble Gallery, Pathway After Left Statue',
             },
+            # 'Debug 99': {
+            #     'Location': 'Colosseum, Entrance',
+            #     'Relic - Form of Mist': True,
+            # },
         }
         with open(os.path.join('build', 'sandbox', 'logic-core.json'), 'w') as logic_core_json:
             json.dump(logic_core, logic_core_json, indent='    ', sort_keys=True)
         skills = json.load(skills_json)
         print('Solving')
-        map_solver = Solver(logic_core, skills)
-        map_solver.solve(51 + 4)
+        map_solver = Solver2(logic_core, skills)
+        # map_solver.solve(51 + 4)
+        map_solver.solve(3, 12)
+        if len(map_solver.results['Wins']) > 0:
+            (winning_layers, winning_game) = map_solver.results['Wins'][-1]
+            print('-------------')
+            print('GOAL REACHED: Layer', winning_layers)
+            print('History')
+            for (layer, location, command_name) in winning_game.history:
+                print('-', layer, location, ':', command_name)
+            print('State')
+            for (key, value) in winning_game.state.items():
+                print('-', key, ':', value)
+            print('-------------')
         # Halt and write files if solution found
         solutions = {
-            'Win Count': map_solver.winning_game_count,
-            'Wins': list(map_solver.winning_games),
-            'Loss Count': map_solver.losing_game_count,
-            'Losses': map_solver.losing_games,
+            'Win Count': len(map_solver.results['Wins']),
+            'Loss Count': len(map_solver.results['Losses']),
             'Solver Version': SOLVER_VERSION,
         }
         with open(os.path.join('build', 'sandbox', 'solutions.json'), 'w') as solutions_json:
             json.dump(solutions, solutions_json, indent='    ', sort_keys=True)
-        print('Wins:', map_solver.winning_game_count)
-        map_solver2 = Solver2(logic_core, skills)
-        map_solver2.solve()
+        # map_solver2 = Solver2(logic_core, skills)
+        # map_solver2.solve()
