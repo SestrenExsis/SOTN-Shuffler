b9e43aded605f0a2d790435875608fe2d9a529c7
SestrenExsis <sestrenexsis@gmail.com>
2025-01-15 17:28:02 -0600

    Draw rooms onto map

--- src/shuffler.py
+++ src/shuffler.py
@@ -9,14 +9,42 @@ import random
 import mapper
 import solver
 
-def get_room_drawing(mapper_core, stage_name, room_id) -> list[str]:
-    result = [
-        '00000',
-        '01110',
-        '01110',
-        '01110',
-        '00000',
-    ]
+def get_room_drawing(mapper_core, room_name) -> list[str]:
+    room = mapper_core['Rooms'][room_name]
+    rows = room['Rows']
+    cols = room['Columns']
+    char = '1'
+    if room_name.startswith('Warp Room '):
+        char = '4'
+    elif 'Save Room' in room_name:
+        char = '5'
+    elif 'Loading Room' in room_name:
+        char = 'c'
+    elif 'Fake Room With Teleporter' in room_name:
+        char = 'a'
+    grid = [['0' for col in range(1 + 4 * cols)] for row in range(1 + 4 * rows)]
+    for row in range(rows):
+        row_span = 4 if row < (rows - 1) else 3
+        for col in range(cols):
+            col_span = 4 if col < (cols - 1) else 3
+            for r in range(row_span):
+                for c in range(col_span):
+                    grid[1 + 4 * row + r][1 + 4 * col + c] = char
+    for node in room['Nodes'].values():
+        row = 2 + 4 * node['Row']
+        col = 2 + 4 * node['Column']
+        if node['Edge'] == 'Top':
+            row -= 2
+        elif node['Edge'] == 'Left':
+            col -= 2
+        elif node['Edge'] == 'Bottom':
+            row += 2
+        elif node['Edge'] == 'Right':
+            col += 2
+        grid[row][col] = '1'
+    result = []
+    for row in range(len(grid)):
+        result.append(''.join(grid[row]))
     return result
 
 if __name__ == '__main__':
@@ -193,13 +221,13 @@ if __name__ == '__main__':
                         'Left': room_left,
                     }
                     # Draw room on castle map drawing grid
-                    room_drawing = get_room_drawing(mapper_core, stage_name, room_name)
+                    room_drawing = get_room_drawing(mapper_core, room_name)
                     for (room_row, row_data) in enumerate(room_drawing):
                         row = 4 * room_top + room_row
                         for (room_col, char) in enumerate(row_data):
                             col = 4 * room_left + room_col
                             if row >= 256 or col >= 256:
-                                print((room_top, room_left), (row, col))
+                                print(room_name, (room_top, room_left), (row, col))
                             castle_map[row][col] = char
                     # Apply Castle Entrance room positions to Castle Entrance Revisited
                     if stage_name == 'Castle Entrance' and room_name not in (
