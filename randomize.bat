897b649fa4b1820dd1a5ae3ede7e58786e5fb540
SestrenExsis <sestrenexsis@gmail.com>
2025-03-20 20:24:02 -0500

    Handle familiar event changes in the Patcher

--- randomize.bat
+++ randomize.bat
@@ -2,7 +2,7 @@
 python lib/SOTN-Patcher/src/sotn_extractor.py "build/patcher/Castlevania - Symphony of the Night (Track 1).bin" "build/patcher/extraction.json" || goto :error
 python lib/SOTN-Patcher/src/sotn_patcher.py "build/patcher/extraction.json" || goto :error
 python src/shuffler.py "data/solver/stage_validations.yaml" || goto :error
-python lib/SOTN-Patcher/src/sotn_patcher.py "build/patcher/extraction.json" --changes="build/shuffler/current-seed.json" --aliases="lib/SOTN-Patcher/data/aliases.yaml" --ppf="build/patcher/current-seed.ppf" || goto :error
+python lib/SOTN-Patcher/src/sotn_patcher.py "build/patcher/extraction.json" --data="lib/SOTN-Patcher/data/" --changes="build/shuffler/current-seed.json" --ppf="build/patcher/current-seed.ppf" || goto :error
 
 goto :EOF
 
