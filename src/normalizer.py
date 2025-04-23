593e7b3d877f7eb0dd207dff6b97c40eb38f9025
SestrenExsis <sestrenexsis@gmail.com>
2025-04-23 05:31:13 -0500

    Normalize a few connections in Underground Caverns

--- src/normalizer.py
+++ src/normalizer.py
@@ -16,6 +16,8 @@ def stamp(room, top, left, grid):
 stages = {
     'Underground Caverns': {
         'Underground Caverns, Crystal Bend',
+        'Underground Caverns, DK Bridge',
+        'Underground Caverns, Exit to Abandoned Mine',
         'Underground Caverns, Long Drop',
         'Underground Caverns, Tall Stairwell',
     },
@@ -26,38 +28,38 @@ rooms = {
         {
             'Layer': 'Foreground',
             'Source': [
-                r'----..----------',
-                r'###@..@#########',
-                r'###@..@#########',
-                r'##@r.=@#########',
-                r'##@...7@@#######',
-                r'##@.....7@@#####',
-                r'##@==.....@#####',
-                r'##@==.....@#####',
-                r'##@.......@#####',
-                r'##@L.....J@#####',
-                r'###@L....@######',
-                r'####@=...@######',
-                r'####@....@######',
-                r'###@r....@######',
-                r'###@.....@######',
-                r'##@r....=@######',
-                r'##@......7@#####',
-                r'##@.......@#####',
-                r'##@.......@#####',
-                r'##@===....7@####',
-                r'##@........7@###',
-                r'##@L........7@@@',
-                r'###@.........vvv',
-                r'###@L...=.......',
-                r'####@L..|.......',
-                r'#####@@L^.o..o..',
-                r'#######@@@@@@@@@',
-                r'################',
-                r'################',
-                r'################',
-                r'################',
-                r'                ',
+                r'----..----------', # r'----..----------',
+                r'###@..@#########', # r'###@..@#########',
+                r'###@..@#########', # r'###@..@#########',
+                r'##@r.=@#########', # r'##@r.=@#########',
+                r'##@...7@@#######', # r'##@...7@@#######',
+                r'##@.....7@@#####', # r'##@.....7@@#####',
+                r'##@.......@#####', # r'##@.......@#####',
+                r'##@==.....@#####', # r'##@==.....@#####',
+                r'                ', # r'##@.......@#####',
+                r'                ', # r'##@L.....J@#####',
+                r'                ', # r'###@L....@######',
+                r'                ', # r'####@=...@######',
+                r'                ', # r'####@....@######',
+                r'                ', # r'###@r....@######',
+                r'                ', # r'###@.....@######',
+                r'                ', # r'##@r....=@######',
+                r'                ', # r'##@......7@#####',
+                r'                ', # r'##@.......@#####',
+                r'                ', # r'##@.......@#####',
+                r'                ', # r'##@===....7@####',
+                r'                ', # r'##@........7@###',
+                r'                ', # r'##@L........7@@@',
+                r'                ', # r'###@.........vvv',
+                r'                ', # r'###@L...=.......',
+                r'                ', # r'####@L..|.......',
+                r'                ', # r'#####@@L^.o..o..',
+                r'                ', # r'#######@@@@@@@@@',
+                r'                ', # r'################',
+                r'                ', # r'################',
+                r'                ', # r'################',
+                r'                ', # r'################',
+                r'                ', # r'                ',
             ],
             'Target': [
                 r'------....------',
@@ -66,8 +68,8 @@ rooms = {
                 r'##@r..====@#####',
                 r'##@.......@#####',
                 r'##@.......@#####',
-                r'                ',
-                r'                ',
+                r'##@.......@#####',
+                r'##@===....@#####',
                 r'                ',
                 r'                ',
                 r'                ',
@@ -97,38 +99,38 @@ rooms = {
         {
             'Layer': 'Background',
             'Source': [
-                r'                ',
-                r'                ',
-                r'                ',
-                r'     =          ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'      0123      ',
-                r'      4567      ',
-                r'      89AB      ',
-                r'      CDEF      ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
+                r'                ', # r'----..----------',
+                r'                ', # r'###@..@#########',
+                r'                ', # r'###@..@#########',
+                r'     =          ', # r'##@r.=@#########',
+                r'                ', # r'##@...7@@#######',
+                r'                ', # r'##@.....7@@#####',
+                r'                ', # r'##@.......@#####',
+                r'   ==           ', # r'##@==.....@#####',
+                r'                ', # r'##@.......@#####',
+                r'                ', # r'##@L.....J@#####',
+                r'                ', # r'###@L....@######',
+                r'     =          ', # r'####@=...@######',
+                r'                ', # r'####@....@######',
+                r'                ', # r'###@r....@######',
+                r'                ', # r'###@.....@######',
+                r'        =       ', # r'##@r....=@######',
+                r'                ', # r'##@......7@#####',
+                r'      0123      ', # r'##@.......@#####',
+                r'      4567      ', # r'##@.......@#####',
+                r'   ===89AB      ', # r'##@===....7@####',
+                r'      CDEF      ', # r'##@........7@###',
+                r'                ', # r'##@L........7@@@',
+                r'                ', # r'###@.........vvv',
+                r'                ', # r'###@L...=.......',
+                r'                ', # r'####@L..|.......',
+                r'                ', # r'#####@@L^.o..o..',
+                r'                ', # r'#######@@@@@@@@@',
+                r'                ', # r'################',
+                r'                ', # r'################',
+                r'                ', # r'################',
+                r'                ', # r'################',
+                r'                ', # r'                ',
             ],
             'Target': [
                 r'                ', # r'------....------',
@@ -137,51 +139,95 @@ rooms = {
                 r'    AB====      ', # r'##@r..====@#####',
                 r'    EFCDEF      ', # r'##@.......@#####',
                 r'    230123      ', # r'##@.......@#####',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
-                r'                ',
+                r'                ', # r'##@.......@#####',
+                r'   ===          ', # r'##@===....@#####',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
+                r'                ', # r'                ',
             ],
         },
     ],
 }
 
-# Underground Caverns, Tall Stairwell
+# Underground Caverns, DK Bridge
 edits = []
-source = get_empty_room(9, 1)
-stamp(source, 8 * 16 + 12, 4, ['##.  .##'])
-target = get_empty_room(9, 1)
-stamp(target, 8 * 16 + 13, 4, ['##.  .##'])
-stamp(target, 8 * 16 + 14, 4, ['##.  .##'])
+source = get_empty_room(1, 4)
+stamp(source, 14, 3 * 16 + 0, ['######@...@#####'])
+stamp(source, 15, 3 * 16 + 0, ['-------...------'])
+target = get_empty_room(1, 4)
+stamp(target, 14, 3 * 16 + 0, ['#####@....@#####'])
+stamp(target, 15, 3 * 16 + 0, ['------....------'])
 edit = {
     'Layer': 'Foreground',
     'Source': source,
     'Target': target,
 }
 edits.append(edit)
-rooms['Underground Caverns, Tall Stairwell'] = edits
+rooms['Underground Caverns, DK Bridge'] = edits
+
+# Underground Caverns, Exit to Abandoned Mine
+edits = []
+source = get_empty_room(1, 1)
+stamp(source, 0, 0, ['   ----.        '])
+stamp(source, 1, 0, ['   ###@.        '])
+stamp(source, 2, 0, ['   ###@.        '])
+stamp(source, 3, 0, ['   ###@.        '])
+stamp(source, 4, 0, ['   ###@=        '])
+stamp(source, 5, 0, ['   @@@r.        '])
+stamp(source, 6, 0, ['   www..        '])
+target = get_empty_room(1, 1)
+stamp(target, 0, 0, ['   ---..        '])
+stamp(target, 1, 0, ['   ##@..        '])
+stamp(target, 2, 0, ['   ##@..        '])
+stamp(target, 3, 0, ['   ##@..        '])
+stamp(target, 4, 0, ['   ##@==        '])
+stamp(target, 5, 0, ['   @@r..        '])
+stamp(target, 6, 0, ['   ww...        '])
+edit = {
+    'Layer': 'Foreground',
+    'Source': source,
+    'Target': target,
+}
+edits.append(edit)
+rooms['Underground Caverns, Exit to Abandoned Mine'] = edits
+
+
+# Underground Caverns, Hidden Crystal Entrance
+# edits = []
+# source = get_empty_room(3, 1)
+# stamp(source, 14, 3 * 16 + 0, ['######@...@#####'])
+# stamp(source, 15, 3 * 16 + 0, ['-------...------'])
+# target = get_empty_room(3, 1)
+# stamp(target, 14, 3 * 16 + 0, ['#####@....@#####'])
+# stamp(target, 15, 3 * 16 + 0, ['------....------'])
+# edit = {
+#     'Layer': 'Foreground',
+#     'Source': source,
+#     'Target': target,
+# }
+# edits.append(edit)
+# rooms['Underground Caverns, Hidden Crystal Entrance'] = edits
 
 # Underground Caverns, Long Drop
 edits = []
@@ -190,12 +236,12 @@ stamp(source, 10 * 16 + 11, 0, ['###@L.......J@##'])
 stamp(source, 10 * 16 + 12, 0, ['###@@.......@@##'])
 stamp(source, 10 * 16 + 13, 0, ['###@@.......@@##'])
 stamp(source, 10 * 16 + 14, 0, ['###@@.......@@##'])
-stamp(source, 10 * 16 + 14, 0, ['-------..-------'])
+stamp(source, 10 * 16 + 15, 0, ['-------..-------'])
 target = get_empty_room(11, 1)
 stamp(target, 10 * 16 + 12, 0, ['           J@#  '])
 stamp(target, 10 * 16 + 13, 0, ['   #@L    J@##  '])
 stamp(target, 10 * 16 + 14, 0, ['   #@@    @@##  '])
-stamp(target, 10 * 16 + 14, 0, ['      ....      '])
+stamp(target, 10 * 16 + 15, 0, ['      ....      '])
 edit = {
     'Layer': 'Foreground',
     'Source': source,
@@ -203,6 +249,26 @@ edit = {
 }
 edits.append(edit)
 rooms['Underground Caverns, Long Drop'] = edits
+
+# Underground Caverns, Tall Stairwell
+edits = []
+source = get_empty_room(9, 1)
+stamp(source, 8 * 16 + 12, 4, ['##.  .##'])
+stamp(source, 8 * 16 + 13, 4, ['        '])
+stamp(source, 8 * 16 + 14, 4, ['        '])
+stamp(source, 8 * 16 + 15, 4, ['---..---'])
+target = get_empty_room(9, 1)
+stamp(target, 8 * 16 + 12, 4, ['        '])
+stamp(target, 8 * 16 + 13, 4, ['##.  .##'])
+stamp(target, 8 * 16 + 14, 4, ['##.  .##'])
+stamp(target, 8 * 16 + 15, 4, ['--.  .--'])
+edit = {
+    'Layer': 'Foreground',
+    'Source': source,
+    'Target': target,
+}
+edits.append(edit)
+rooms['Underground Caverns, Tall Stairwell'] = edits
 
 def normalize(room_name: str):
     edits = []
