50fddf6c3e748467f414ea596f945b1b301a4de3
SestrenExsis <sestrenexsis@gmail.com>
2025-08-22 17:37:15 -0500

    Examine subsets and supersets when adding locks

--- src/integrator.py
+++ src/integrator.py
@@ -1,15 +1,11 @@
 # External libraries
 import collections
-import copy
-import hashlib
-import itertools
 import json
 import os
 import time
 import yaml
 
 # Local libraries
-import mapper
 import solver
 
 entry_points = {
@@ -71,7 +67,6 @@ goals__all_checks = {
 }
 
 all_progressions = {
-    # 'Empty Hand': {},
     'Cube of Zoe': {
         'Progression - Item Materialization': True,
         'Relic - Cube of Zoe': True,
@@ -84,26 +79,15 @@ all_progressions = {
         'Progression - Echolocation': True,
         'Relic - Echo of Bat': True,
     },
-    # 'Faerie Card': {
-    #     'Progression - Summon Faerie Familiar': True,
-    #     'Relic - Faerie Card': True,
-    # },
     'Form of Mist': {
         'Progression - Mid-Air Reset': True,
         'Progression - Mist Transformation': True,
         'Relic - Form of Mist': True,
     },
-    # 'Gold Ring + Silver Ring': {
-    #     'Item - Gold Ring': 1,
-    #     'Item - Silver Ring': 1,
-    # },
     'Gravity Boots': {
         'Progression - Gravity Jump': True,
         'Relic - Gravity Boots': True,
     },
-    # 'Holy Glasses': {
-    #     'Item - Holy Glasses': 1,
-    # },
     'Holy Symbol': {
         'Progression - Protection From Water': True,
         'Relic - Holy Symbol': True,
@@ -125,14 +109,6 @@ all_progressions = {
         'Progression - Longer Mist Duration': True,
         'Relic - Power of Mist': True,
     },
-    # 'Power of Wolf': {
-    #     'Progression - Faster Wolf Run Speed': True,
-    #     'Relic - Power of Wolf': True,
-    # },
-    # 'Skill of Wolf': {
-    #     'Progression - Wolf Charge Attack': True,
-    #     'Relic - Skill of Wolf': True,
-    # },
     'Soul of Bat': {
         'Progression - Bat Transformation': True,
         'Progression - Mid-Air Reset': True,
@@ -146,196 +122,15 @@ all_progressions = {
     'Spike Breaker': {
         'Item - Spike Breaker': 1,
     },
-    # 'Sword Card': {
-    #     'Progression - Summon Sword Familiar': True,
-    #     'Relic - Sword Card': True,
-    # },
-    # 'All': {
-    #     'Progression - Item Materialization': True,
-    #     'Relic - Cube of Zoe': True,
-    #     'Progression - Summon Demon Familiar': True,
-    #     'Relic - Demon Card': True,
-    #     'Progression - Echolocation': True,
-    #     'Relic - Echo of Bat': True,
-    #     'Progression - Mid-Air Reset': True,
-    #     'Progression - Mist Transformation': True,
-    #     'Relic - Form of Mist': True,
-    #     'Item - Gold Ring': 1,
-    #     'Item - Silver Ring': 1,
-    #     'Progression - Gravity Jump': True,
-    #     'Relic - Gravity Boots': True,
-    #     'Item - Holy Glasses': 1,
-    #     'Progression - Protection From Water': True,
-    #     'Relic - Holy Symbol': True,
-    #     'Progression - Unlock Blue Doors': True,
-    #     'Relic - Jewel of Open': True,
-    #     'Progression - Double Jump': True,
-    #     'Progression - Mid-Air Reset': True,
-    #     'Relic - Leap Stone': True,
-    #     'Progression - Summon Ferryman': True,
-    #     'Relic - Merman Statue': True,
-    #     'Progression - Longer Mist Duration': True,
-    #     'Relic - Power of Mist': True,
-    #     'Progression - Faster Wolf Run Speed': True,
-    #     'Relic - Power of Wolf': True,
-    #     'Progression - Wolf Charge Attack': True,
-    #     'Relic - Skill of Wolf': True,
-    #     'Progression - Bat Transformation': True,
-    #     'Progression - Mid-Air Reset': True,
-    #     'Relic - Soul of Bat': True,
-    #     'Progression - Mid-Air Reset': True,
-    #     'Progression - Wolf Transformation': True,
-    #     'Relic - Soul of Wolf': True,
-    #     'Item - Spike Breaker': 1,
-    # },
 }
 
-def validate(mapper_core, changes, skills, validation):
-    logic_core = mapper.LogicCore(mapper_core, changes).get_core()
-    for (state_key, state_value) in validation['State'].items():
-        logic_core['State'][state_key] = state_value
-    logic_core['Goals'] = validation['Goals']
-    map_solver = solver.Solver(logic_core, skills)
-    solver_config = validation['Solver']
-    map_solver.initial_seed = solver_config.get('Initial Seed', 0)
-    map_solver.debug = solver_config.get('Debug', False)
-    validation_passed = True
-    for attempt_id in range(solver_config.get('Attempts', 1)):
-        if solver_config['Approach'] == 'Random Exploration':
-            cycle_limit = solver_config.get('Limit', 1_999)
-            map_solver.solve_via_random_exploration(cycle_limit, stage_name)
-        elif solver_config['Approach'] == 'Steps':
-            step_limit = solver_config.get('Limit', 100)
-            map_solver.solve_via_relaxed_steps(step_limit, stage_name)
-        else:
-            raise Exception('Unrecognized approach:', validation['Solver']['Approach'])
-    for result_name in ('Wins', 'Losses', 'Withdrawals'):
-        if result_name in solver_config:
-            minimum = solver_config[result_name].get('Minimum', float('-inf'))
-            maximum = solver_config[result_name].get('Maximum', float('inf'))
-            result_count = len(map_solver.results[result_name])
-            if minimum <= result_count <= maximum:
-                pass
-            else:
-                validation_passed = False
-                break
-    result = validation_passed
-    return result
-
-def analyze_stage(stage_name, hash_of_rooms, skills):
-    mapper_core = mapper.MapperData().get_core()
-    with open(os.path.join('build', 'shuffler', stage_name, hash_of_rooms + '.json')) as mapper_data_json:
-        mapper_data = json.load(mapper_data_json)
-        mapper_data_json.close()
-    current_mapper = mapper.Mapper(mapper_core, stage_name, mapper_data['Seed'])
-    current_mapper.generate(mapper.stages[stage_name])
-    current_mapper.stage.normalize_bounds()
-    current_mapper__stage_changes = current_mapper.stage.get_changes()
-    hash_of_rooms = hashlib.sha256(
-        json.dumps(current_mapper__stage_changes['Rooms'], sort_keys=True).encode()
-    ).hexdigest()
-    assert hash_of_rooms == mapper_data['Hash of Rooms']
-    changes = {
-        'Stages': {
-            stage_name: current_mapper__stage_changes,
-        },
-    }
-    validation_template = {
-        'Solver': {
-            'Approach': 'Steps',
-            'Attempts': 1,
-            'Limit': 99,
-            'Initial Seed': 1,
-            'Debug': False,
-            'Wins': {
-                'Minimum': 1,
-            },
-        },
-        'State': {},
-        'Goals': {},
-    }
-    analysis = {}
-    work = []
-    for entry_point_id in sorted(entry_points.keys()):
-        for exit_point_id in sorted(entry_points.keys()):
-            work.append((entry_point_id, exit_point_id))
-    for entry_point_id in sorted(entry_points.keys()):
-        for goal_id in sorted(goals.keys()):
-            work.append((entry_point_id, goal_id))
-    for goal_id in sorted(goals.keys()):
-        for exit_point_id in sorted(entry_points.keys()):
-            work.append((goal_id, exit_point_id))
-    for goal_id in sorted(goals.keys()):
-        for other_goal_id in sorted(goals.keys()):
-            work.append((goal_id, other_goal_id))
-    requirement_ids = list(sorted(all_progressions.keys()))
-    while len(work) > 0:
-        (entry_point_id, exit_point_id) = work.pop()
-        if entry_point_id == exit_point_id:
-            continue
-        entry_point = entry_points.get(entry_point_id, {}) if entry_point_id in entry_points else goals.get(entry_point_id, {})
-        exit_point = entry_points.get(exit_point_id, {}) if exit_point_id in entry_points else goals.get(exit_point_id, {})
-        all_requirements = set()
-        print('', (entry_point_id, exit_point_id))
-        for requirement_count in range(10): # len(requirement_ids) + 1):
-            skip_count = 0
-            check_count = 0
-            empty_hand_count = 0
-            if requirement_count == 0:
-                check_count += 1
-                validation = copy.deepcopy(validation_template)
-                for (key, value) in entry_point.items():
-                    validation['State'][key] = value
-                goal = {}
-                for (key, value) in exit_point.items():
-                    goal[key] = value
-                validation['Goals']['Goal'] = goal
-                result = validate(mapper_core, changes, skills, validation)
-                if result:
-                    all_requirements.add(('Empty Hand', ))
-            else:
-                for curr_requirements in itertools.combinations(requirement_ids, requirement_count):
-                    check_count += 1
-                    if ('Empty Hand', ) in all_requirements:
-                        empty_hand_count += 1
-                        continue
-                    subset_found = False
-                    set_of_curr_requirements = set(curr_requirements)
-                    for prev_requirements in all_requirements:
-                        if set_of_curr_requirements.issuperset(set(prev_requirements)):
-                            subset_found = True
-                            break
-                    if subset_found:
-                        skip_count += 1
-                        continue
-                    validation = copy.deepcopy(validation_template)
-                    for (key, value) in entry_point.items():
-                        validation['State'][key] = value
-                    for requirement_id in curr_requirements:
-                        for (key, value) in requirements[requirement_id].items():
-                            validation['State'][key] = value
-                    goal = {}
-                    for (key, value) in exit_point.items():
-                        goal[key] = value
-                    validation['Goals']['Goal'] = goal
-                    result = validate(mapper_core, changes, skills, validation)
-                    if result:
-                        if (entry_point_id, exit_point_id) not in analysis:
-                            analysis[(entry_point_id, exit_point_id)] = set()
-                        all_requirements.add(curr_requirements)
-            print('  ', requirement_count, check_count, skip_count, empty_hand_count)
-        analysis[(entry_point_id, exit_point_id)] = all_requirements
-    for (entry_point_id, exit_point_id) in sorted(analysis.keys()):
-        print('', (entry_point_id, exit_point_id))
-        for requirements_key in sorted(analysis[(entry_point_id, exit_point_id)]):
-            print('  - ', requirements_key)
-    return result
-
-def get_reachable_checks(map_solver, progression_ids: set, custom_goal: dict=None):
+def get_reachable_checks(map_solver, progression_ids: set):
     progression = {}
     for progression_id in progression_ids:
         for (key, value) in all_progressions[progression_id].items():
             progression[key] = value
+    for (key, value) in progression.items():
+        print(key, value)
     # Find all locations potentially reachable without additional progression
     game__init = map_solver.current_game.clone()
     for (key, value) in progression.items():
@@ -407,12 +202,38 @@ if __name__ == '__main__':
                 else ' + '.join(sorted(progression_items))
             )
             memo[progression_key] = reachable_checks
-            print(' -', len(reachable_checks), progression_key)
+            print(' -', len(reachable_checks), progression_key, '--> DONE' if len(reachable_checks) >= len(all_checks) else '')
             if len(reachable_checks) >= len(all_checks):
-                print('  ', 'ALL LOCATIONS REACHABLE FOR', progression_key, '!!!')
                 continue
             for progression_id in sorted(all_progressions):
                 if len(progression_items) > 0 and progression_id <= max(progression_items):
                     continue
                 # print('---- ', progression_id, progression_items)
                 work.appendleft(progression_items.union({ progression_id, }))
+        location_locks = {}
+        def F(d, k, v):
+            # d[k].add(v)
+            m = set(v.split(' + '))
+            add_value_ind = True
+            values_to_remove = set()
+            for v2 in d[k]:
+                m2 = set(v2.split(' + '))
+                if (v == 'Empty Hand') or (m.issubset(m2)):
+                    values_to_remove.add(v2)
+                if (v2 == 'Empty Hand') or (m2.issubset(m)):
+                    add_value_ind = False
+            for value_to_remove in values_to_remove:
+                d[k].remove(value_to_remove)
+            if add_value_ind:
+                d[k].add(v)
+        for (progression_key, reachable_checks) in memo.items():
+            for check in reachable_checks:
+                if check not in location_locks:
+                    location_locks[check] = set()
+                F(location_locks, check, progression_key) # location_locks[check].add(progression_key)
+        print('\nLocation Locks -', len(location_locks), 'in total')
+        for check in sorted(location_locks):
+            progression_keys = location_locks[check]
+            print('', check)
+            for progression_key in sorted(progression_keys):
+                print('  -', progression_key)
