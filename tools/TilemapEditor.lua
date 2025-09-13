7d6fdfc0b4651e9cce52254d2a020f90120bf8d2
SestrenExsis <sestrenexsis@gmail.com>
2025-09-13 08:20:10 -0500

    Enable reading of background and foreground tiles

--- tools/TilemapEditor.lua
+++ tools/TilemapEditor.lua
@@ -1,5 +1,5 @@
 
--- Uses lots of tile-finding math from MottZilla's SotN_TileMapFind script
+-- Special thanks to Mottzilla for their SotN_TileMapFind script, which formed the basis for a lot of the math and address lookups
 
 local P = {}
 TilemapEditor = P
@@ -10,29 +10,9 @@ P.Memory = {
         Type = "u8",
         Value = nil,
     },
-    RoomWidth = {
-        Address = 0x000730C8,
-        Type = "u16le",
-        Value = nil,
-    },
-    BackgroundPointer = {
+    ForegroundPointer = {
         Address = 0x00073084,
         Type = "u32le",
-        Value = nil,
-    },
-    ForegroundPointer = {
-        Address = 0x000730D8,
-        Type = "u32le",
-        Value = nil,
-    },
-    ScrollX = {
-        Address = 0x0007308E,
-        Type = "u16le",
-        Value = nil,
-    },
-    ScrollY = {
-        Address = 0x00073092,
-        Type = "u16le",
         Value = nil,
     },
     PlayerX = {
@@ -45,6 +25,26 @@ P.Memory = {
         Type = "u32le",
         Value = nil,
     },
+    RoomHeight = {
+        Address = 0x000730CC,
+        Type = "u16le",
+        Value = nil,
+    },
+    RoomWidth = {
+        Address = 0x000730C8,
+        Type = "u16le",
+        Value = nil,
+    },
+    ScrollX = {
+        Address = 0x0007308E,
+        Type = "u16le",
+        Value = nil,
+    },
+    ScrollY = {
+        Address = 0x00073092,
+        Type = "u16le",
+        Value = nil,
+    },
 }
 
 P.MemoryKeys = {}
@@ -54,54 +54,93 @@ end
 table.sort(P.MemoryKeys)
 
 P.text = function(__col, __row, __message, __color)
-    local font_height = 12
-    local font_width = 8
-    if __color == nil then
-        __color = 0xff999999
+    local font_height = 12 * P.scale
+    local font_width = 8 * P.scale
+    if (__color == nil) then
+        __color = 0xFF999999
     end
     local x = font_width * (__col + 0.5)
     local y = font_height * (__row + 0.5)
-    P.canvas.DrawText(x, y, __message, __color, 0xff000000, font_height)
+    P.canvas.DrawText(x, y, __message, __color, 0xFF000000, font_height)
+end
+
+P.update_input = function()
+    P.prev_keys = P.curr_keys
+    P.curr_keys = input.get()
+    P.prev_mouse = P.curr_mouse
+    P.curr_mouse = input.getmouse()
+    -- Update columns
+    P.prev_col = P.curr_col
+    P.scroll_col = math.floor(P.Memory.ScrollX.Value / 16)
+    P.mouse_col = math.floor((P.curr_mouse.X + (0xF & P.Memory.ScrollX.Value) + 2) / 16) - 1
+    P.curr_col = (P.mouse_col + P.scroll_col)
+    -- Update rows
+    P.prev_row = P.curr_row
+    P.scroll_row = math.floor(P.Memory.ScrollY.Value / 16)
+    P.mouse_row = math.floor((P.curr_mouse.Y + (0xF & P.Memory.ScrollY.Value) + 0) / 16)
+    P.curr_row = (P.mouse_row + P.scroll_row)
+    if (P.curr_keys.Z and (P.prev_keys.Z == nil or P.prev_keys.Z == false)) then
+        if P.mode == "READ" then
+            P.mode = "WRITE"
+        else
+            P.mode = "READ"
+        end
+    end
+    local width_in_tiles = (P.Memory.RoomWidth.Value / 16)
+    local height_in_tiles = (P.Memory.RoomHeight.Value / 16)
+    local tile_offset = 2 * ((width_in_tiles * P.curr_row) + P.curr_col)
+    local fg_offset = (0x1FFFFF & P.Memory.ForegroundPointer.Value)
+    if (P.curr_mouse.Left == true) then
+        if (P.mode == "READ") then
+            local tilemap_offset = fg_offset + 2 * width_in_tiles * height_in_tiles
+            P.bg_tile_data = memory.read_u16_le(tilemap_offset + tile_offset)
+        end
+    end
+    if (P.curr_mouse.Right == true) then
+        if (P.mode == "READ") then
+            local tilemap_offset = fg_offset
+            P.fg_tile_data = memory.read_u16_le(tilemap_offset + tile_offset)
+        end
+    end
 end
 
 P.draw = function()
     gui.clearGraphics()
     P.canvas.Clear(0xFF000000)
-    P.prev_mouse = P.curr_mouse
-    P.curr_mouse = input.getmouse()
-    local row = 0
+    P.text(0, 0, "Mode")
+    P.text(20, 0, P.mode)
+    P.text(0, 1, "TileX")
+    P.text(20, 1, P.curr_col)
+    P.text(0, 2, "TileY")
+    P.text(20, 2, P.curr_row)
+    P.text(0, 3, "Stored Background")
+    P.text(20, 3, bizstring.hex(P.bg_tile_data))
+    P.text(0, 4, "Stored Foreground")
+    P.text(20, 4, bizstring.hex(P.fg_tile_data))
     for index, value_name in pairs(P.MemoryKeys) do
         local value = P.Memory[value_name]
-        P.text(0, index, value_name)
-        P.text(20, index, value.Type)
-        P.text(30, index, bizstring.hex(value.Value))
-        row = row + 1
+        P.text(0, index + 6, value_name)
+        P.text(20, index + 6, value.Type)
+        P.text(30, index + 6, bizstring.hex(value.Value))
     end
-    local scroll_col = math.floor(P.Memory.ScrollX.Value / 16)
-    local mouse_col = math.floor((P.curr_mouse.X + 2 + (0xF & P.Memory.ScrollX.Value) ) / 16) - 1
-    local tile_col = (mouse_col + scroll_col)
-    local scroll_row = math.floor(P.Memory.ScrollY.Value / 16)
-    local mouse_row = math.floor((P.curr_mouse.Y + 0 + ((0xF & P.Memory.ScrollY.Value))) / 16)
-    local tile_row = (mouse_row + scroll_row)
-    P.text(0, #P.MemoryKeys + 2, "TileX")
-    P.text(30, #P.MemoryKeys + 2, tile_col)
-    P.text(0, #P.MemoryKeys + 3, "TileY")
-    P.text(30, #P.MemoryKeys + 3, tile_row)
-    local bx0 = 16 * (tile_col - scroll_col) - (0xF & P.Memory.ScrollX.Value) + 14
-    local by0 = 16 * (tile_row - scroll_row) - (0xF & P.Memory.ScrollY.Value) - 1
-    gui.drawBox(bx0, by0, bx0 + 17, by0 + 17, 0xFFFF0000, 0x00000000)
-    gui.drawPixel(left, top, 0xFFFFFFFF)
+    local left = 16 * (P.curr_col - P.scroll_col) - (0xF & P.Memory.ScrollX.Value) + 14
+    local top = 16 * (P.curr_row - P.scroll_row) - (0xF & P.Memory.ScrollY.Value) - 1
+    local box_color = 0xFFFFFF00
+    if (P.mode == "WRITE") then
+        box_color = 0xFFFF0000
+    end
+    gui.drawBox(left, top, left + 17, top + 17, box_color, 0x00000000)
     P.canvas.Refresh()
 end
 
 P.update = function()
     P.room_pointer = (0x1FFFFF & mainmemory.read_u32_le(0x73084))
     for v_name, v_props in pairs(P.Memory) do
-        if v_props.Type == "u8" then
+        if (v_props.Type == "u8") then
             v_props.Value = mainmemory.read_u8(v_props.Address)
-        elseif v_props.Type == "u16le" then
+        elseif (v_props.Type == "u16le") then
             v_props.Value = mainmemory.read_u16_le(v_props.Address)
-        elseif v_props.Type == "u32le" then
+        elseif (v_props.Type == "u32le") then
             v_props.Value = mainmemory.read_u32_le(v_props.Address)
         else
             v_props.Value = mainmemory.read_u32_le(v_props.Address)
@@ -112,24 +151,37 @@ end
 event.unregisterbyname("TilemapEditor__update")
 event.onframeend(P.update, "TilemapEditor__update")
 
-gui.defaultBackground(0xffff0000)
-
-P.room_pointer = 0
-P.prev_mouse = input.getmouse()
-P.curr_mouse = P.prev_mouse
-P.data = {}
-P.row = 0
-P.col = 0
-P.scale = 2
-P.cursor = 0
-
-P.canvas_width = P.scale * (256)
-P.canvas_height = P.scale * (128)
+gui.defaultBackground(0xFFFF0000)
+-- Mouse and keyboard input
+P.curr_keys = input.get()
+P.prev_keys = P.curr_keys
+P.curr_mouse = input.getmouse()
+P.prev_mouse = P.curr_mouse
+-- Scroll coordinates are how many tiles the screen has scrolled from the top-left corner of the room
+P.scroll_col = 0
+P.scroll_row = 0
+-- Mouse coordinates are how many tiles the mouse is from the top-left corner from the screen
+P.mouse_col = 0
+P.mouse_row = 0
+-- Tile coordinates are the sum of Mouse coordinates + Scroll coordinates
+P.curr_col = 0
+P.curr_row = 0
+P.prev_col = curr_col
+P.prev_row = curr_row
+-- Other editor-specific variables
+P.mode = "READ"
+P.bg_tile_data = 0x0000
+P.fg_tile_data = 0x0000
+-- GUI variables
+P.scale = 1.5
+P.canvas_width = P.scale * (400)
+P.canvas_height = P.scale * (200)
 P.canvas = gui.createcanvas(P.canvas_width, P.canvas_height, P.scale * 4, P.scale * 4)
 P.canvas.SetTitle("TilemapEditor")
 
 P.update()
 while true do
     emu.yield()
+    P.update_input()
     P.draw()
 end
