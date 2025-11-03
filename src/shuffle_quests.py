7ea9e711d31f96a6c11819affd1502a7e3f9e464
SestrenExsis <sestrenexsis@gmail.com>
2025-11-02 21:37:27 -0600

    Organize current quest shuffling into discrete functions

--- src/shuffle_quests.py
+++ src/shuffle_quests.py
@@ -3,13 +3,79 @@ import argparse
 import random
 import yaml
 
-def shuffle_relics_and_required_items(quests, seed, relic_pool: str='Vanilla'):
+def get_quest_targets__significant_locations__vanilla(quests, seed):
+    rng = random.Random(seed)
+    quest_targets = []
+    while True:
+        quest_targets = []
+        for quest_source_name in list(sorted(quests.get('Sources', {}))):
+            if 'Significant' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            quest_target_name = quests['Sources'][quest_source_name]['Target Reward']
+            quest_targets.append(quest_target_name)
+        rng.shuffle(quest_targets)
+        valid_ind = True
+        index = 0
+        for quest_source_name in list(sorted(quests['Sources'].keys())):
+            if 'Significant' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            quest_target_type = quest_targets[-index - 1].split(' - ')[0]
+            if quest_target_type not in quests['Sources'][quest_source_name].get('Tags', []):
+                valid_ind = False
+                break
+            index += 1
+        if valid_ind:
+            break
+    result = quest_targets
+    return result
+
+def get_quest_targets__significant_locations__skill_expression(quests, seed):
+    rng = random.Random(seed)
+    quest_targets = []
+    while True:
+        quest_targets = []
+        for quest_source_name in list(sorted(quests.get('Sources', {}))):
+            if 'Significant' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            quest_target_name = quests['Sources'][quest_source_name]['Target Reward']
+            if quest_target_name in (
+                'Power of Mist',
+            ):
+                quest_target_name = 'Demon Card'
+            quest_targets.append(quest_target_name)
+        rng.shuffle(quest_targets)
+        valid_ind = True
+        index = 0
+        for quest_source_name in list(sorted(quests['Sources'].keys())):
+            if 'Significant' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            quest_target_type = quest_targets[-index - 1].split(' - ')[0]
+            if quest_target_type not in quests['Sources'][quest_source_name].get('Tags', []):
+                valid_ind = False
+                break
+            index += 1
+        if valid_ind:
+            break
+    result = quest_targets
+    return result
+
+def get_quest_targets__significant_locations__racing(quests, seed):
     rng = random.Random(seed)
     quest_targets = []
     while True:
         duplicate_relics = []
         for quest_source_name in list(sorted(quests['Sources'].keys())):
-            if quests['Sources'][quest_source_name].get('Logic Level', 'Optional') != 'Required':
+            if 'Significant' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
                 continue
             quest_target_name = quests['Sources'][quest_source_name]['Target Reward']
             if not quest_target_name.startswith('Relic'):
@@ -20,18 +86,22 @@ def shuffle_relics_and_required_items(quests, seed, relic_pool: str='Vanilla'):
         rng.shuffle(duplicate_relics)
         quest_targets = []
         for quest_source_name in list(sorted(quests.get('Sources', {}))):
-            if quests['Sources'][quest_source_name].get('Logic Level', 'Optional') != 'Required':
+            if 'Significant' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
                 continue
             quest_target_name = quests['Sources'][quest_source_name]['Target Reward']
             # If Racing Relic pool is enabled, replace low priority relics with a random duplicate of a better one
-            if relic_pool == 'Racing' and 'Low Priority - Racing' in quests['Targets'][quest_target_name].get('Tags', {}):
+            if 'Low Priority - Racing' in quests['Targets'][quest_target_name].get('Tags', {}):
                 quest_target_name = duplicate_relics.pop()
             quest_targets.append(quest_target_name)
         rng.shuffle(quest_targets)
         valid_ind = True
         index = 0
         for quest_source_name in list(sorted(quests['Sources'].keys())):
-            if quests['Sources'][quest_source_name].get('Logic Level', 'Optional') != 'Required':
+            if 'Significant' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
                 continue
             quest_target_type = quest_targets[-index - 1].split(' - ')[0]
             if quest_target_type not in quests['Sources'][quest_source_name].get('Tags', []):
@@ -40,82 +110,85 @@ def shuffle_relics_and_required_items(quests, seed, relic_pool: str='Vanilla'):
             index += 1
         if valid_ind:
             break
-    for quest_source_name in list(sorted(quests.get('Sources', {}))):
-        if quests['Sources'][quest_source_name].get('Logic Level', 'Optional') != 'Required':
-            continue
-        quest_target_name = quest_targets.pop()
-        quests['Sources'][quest_source_name]['Target Reward'] = quest_target_name
+    result = quest_targets
+    return result
 
-def shuffle_items(quests, seed, item_pool: str='Vanilla'):
+def get_quest_targets__other_locations__vanilla(quests, seed):
     rng = random.Random(seed)
     quest_targets = []
     for quest_source_name in list(sorted(quests.get('Sources', {}))):
-        if 'Item' not in quests['Sources'][quest_source_name].get('Tags', {}):
+        if 'Significant' in quests['Sources'][quest_source_name].get('Tags', {}):
             continue
-        if quests['Sources'][quest_source_name].get('Logic Level', 'Optional') == 'Required':
+        if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
             continue
         quest_target_name = quests['Sources'][quest_source_name]['Target Reward']
         quest_targets.append(quest_target_name)
-    if item_pool == 'Racing':
-        rng.shuffle(quest_targets)
-        item_bin_counts = {
-            'Bomb': 2,
-            'Thrown': 2,
-            'Food': 2,
-            'Buff': 2,
-            'Healing': 1,
-        }
-        # Assign each placeholder an arbitrary item from the item bin
-        placeholders = {}
-        for item_bin_name in sorted(item_bin_counts.keys()):
-            for bin_id in range(1, item_bin_counts[item_bin_name] + 1):
-                placeholder_name = 'Placeholder - ' + item_bin_name + ' ' + str(bin_id)
-                placeholders[placeholder_name] = (item_bin_name, None)
-        chosen_items = set()
-        for placeholder_name in sorted(placeholders.keys()):
-            (item_bin_name, chosen_item) = placeholders[placeholder_name]
-            if chosen_item is not None:
-                continue
-            for i in range(len(quest_targets)):
-                quest_target_name = quest_targets[i]
-                if 'Low Priority - Racing' in quests['Targets'][quest_target_name].get('Tags', {}):
-                    continue
-                if item_bin_name not in quests['Targets'][quest_target_name].get('Tags', {}):
-                    continue
-                if quest_targets[i] in chosen_items:
-                    continue
-                placeholders[placeholder_name] = (item_bin_name, quest_targets[i])
-                chosen_items.add(quest_targets[i])
-                break
+    rng.shuffle(quest_targets)
+    result = quest_targets
+    return result
+
+def get_quest_targets__other_locations__racing(quests, seed):
+    rng = random.Random(seed)
+    quest_targets = []
+    for quest_source_name in list(sorted(quests.get('Sources', {}))):
+        if 'Significant' in quests['Sources'][quest_source_name].get('Tags', {}):
+            continue
+        if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
+            continue
+        quest_target_name = quests['Sources'][quest_source_name]['Target Reward']
+        quest_targets.append(quest_target_name)
+    rng.shuffle(quest_targets)
+    item_bin_counts = {
+        'Bomb': 2,
+        'Thrown': 2,
+        'Food': 2,
+        'Buff': 2,
+        'Healing': 1,
+    }
+    # Assign each placeholder an arbitrary item from the item bin
+    placeholders = {}
+    for item_bin_name in sorted(item_bin_counts.keys()):
+        for bin_id in range(1, item_bin_counts[item_bin_name] + 1):
+            placeholder_name = 'Placeholder - ' + item_bin_name + ' ' + str(bin_id)
+            placeholders[placeholder_name] = (item_bin_name, None)
+    chosen_items = set()
+    for placeholder_name in sorted(placeholders.keys()):
+        (item_bin_name, chosen_item) = placeholders[placeholder_name]
+        if chosen_item is not None:
+            continue
         for i in range(len(quest_targets)):
             quest_target_name = quest_targets[i]
-            # Chance of replacing a Low Priority - Racing item with something else
             if 'Low Priority - Racing' in quests['Targets'][quest_target_name].get('Tags', {}):
-                chance = rng.random()
-                if chance < 0.025:
-                    quest_targets[i] = 'Item - Library Card'
-                elif chance < 0.05:
-                    quest_targets[i] = 'Item - Manna Prism'
-                elif chance < 0.80:
-                    (item_bin_name, assigned_quest_target) = rng.choice(list(sorted(placeholders.values())))
+                continue
+            if item_bin_name not in quests['Targets'][quest_target_name].get('Tags', {}):
+                continue
+            if quest_targets[i] in chosen_items:
+                continue
+            placeholders[placeholder_name] = (item_bin_name, quest_targets[i])
+            chosen_items.add(quest_targets[i])
+            break
+    for i in range(len(quest_targets)):
+        quest_target_name = quest_targets[i]
+        # Chance of replacing a Low Priority - Racing item with something else
+        if 'Low Priority - Racing' in quests['Targets'][quest_target_name].get('Tags', {}):
+            chance = rng.random()
+            if chance < 0.025:
+                quest_targets[i] = 'Item - Library Card'
+            elif chance < 0.05:
+                quest_targets[i] = 'Item - Manna Prism'
+            elif chance < 0.80:
+                (item_bin_name, assigned_quest_target) = rng.choice(list(sorted(placeholders.values())))
+                quest_targets[i] = assigned_quest_target
+        else:
+            # Override items found in a placeholder bin to the binned item
+            for (item_bin_name, assigned_quest_target) in placeholders.values():
+                if item_bin_name in quests['Targets'][quest_target_name].get('Tags', {}):
                     quest_targets[i] = assigned_quest_target
-            else:
-                # Override items found in a placeholder bin to the binned item
-                for (item_bin_name, assigned_quest_target) in placeholders.values():
-                    if item_bin_name in quests['Targets'][quest_target_name].get('Tags', {}):
-                        quest_targets[i] = assigned_quest_target
-                        break
-    else:
-        rng.shuffle(quest_targets)
-    for quest_source_name in list(sorted(quests.get('Sources', {}))):
-        if 'Item' not in quests['Sources'][quest_source_name].get('Tags', {}):
-            continue
-        if quests['Sources'][quest_source_name].get('Logic Level', 'Optional') == 'Required':
-            continue
-        quest_target = quest_targets.pop()
-        quests['Sources'][quest_source_name]['Target Reward'] = quest_target
+                    break
+    result = quest_targets
+    return result
 
-def shuffle_enemy_drops(quests, seed):
+def get_quest_targets__enemy_drops__vanilla(quests, seed):
     rng = random.Random(seed)
     quest_targets = []
     for quest_source_name in list(sorted(quests.get('Sources', {}))):
@@ -124,13 +197,10 @@ def shuffle_enemy_drops(quests, seed):
         quest_target_name = quests['Sources'][quest_source_name]['Target Reward']
         quest_targets.append(quest_target_name)
     rng.shuffle(quest_targets)
-    for quest_source_name in list(sorted(quests.get('Sources', {}))):
-        if 'Enemy Drop' not in quests['Sources'][quest_source_name].get('Tags', {}):
-            continue
-        quest_target = quest_targets.pop()
-        quests['Sources'][quest_source_name]['Target Reward'] = quest_target
+    result = quest_targets
+    return result
 
-def replace_special_items(quests, seed):
+def replace_special_items__vanilla(quests, seed):
     rng = random.Random(seed)
     pools = {}
     for quest_source_name in list(sorted(quests.get('Sources', {}))):
@@ -177,19 +247,48 @@ def main(quests, initial_seed, quest_settings):
     quest_reward_ind = False
     if quest_settings.get('Shuffle relics', False):
         relic_pool = quest_settings.get('Relic pool', 'Vanilla')
-        shuffle_relics_and_required_items(quests, rng['Relics'], relic_pool)
+        quest_targets = None
+        if relic_pool == 'Racing':
+            quest_targets = get_quest_targets__significant_locations__racing(quests, rng['Relics'])
+        elif relic_pool == 'Skill Expression':
+            quest_targets = get_quest_targets__significant_locations__skill_expression(quests, rng['Relics'])
+        else:
+            quest_targets = get_quest_targets__significant_locations__vanilla(quests, rng['Relics'])
+        for quest_source_name in list(sorted(quests.get('Sources', {}))):
+            if 'Significant' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            quest_target_name = quest_targets.pop()
+            quests['Sources'][quest_source_name]['Target Reward'] = quest_target_name
         quest_reward_ind = True
     if quest_settings.get('Shuffle items', False):
         item_pool = quest_settings.get('Item pool', 'Vanilla')
-        shuffle_items(quests, rng['Found Items'], item_pool)
+        quest_targets = None
+        if relic_pool == 'Racing':
+            quest_targets = get_quest_targets__other_locations__racing(quests, rng['Found Items'])
+        else:
+            quest_targets = get_quest_targets__other_locations__vanilla(quests, rng['Found Items'])
+        for quest_source_name in list(sorted(quests.get('Sources', {}))):
+            if 'Significant' in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            if 'Location Check' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            quest_target = quest_targets.pop()
+            quests['Sources'][quest_source_name]['Target Reward'] = quest_target
         quest_reward_ind = True
     if quest_settings.get('Shuffle enemy drops', False):
         enemy_drop_pool = quest_settings.get('Enemy drop pool', 'Vanilla')
-        shuffle_enemy_drops(quests, rng['Enemy Drops'], enemy_drop_pool)
+        quest_targets = get_quest_targets__enemy_drops__vanilla(quests, rng['Enemy Drops'])
+        for quest_source_name in list(sorted(quests.get('Sources', {}))):
+            if 'Enemy Drop' not in quests['Sources'][quest_source_name].get('Tags', {}):
+                continue
+            quest_target = quest_targets.pop()
+            quests['Sources'][quest_source_name]['Target Reward'] = quest_target
         quest_reward_ind = True
     if quest_settings.get('Shuffle special items', False):
         special_item_pool = quest_settings.get('Special item pool', 'Vanilla')
-        replace_special_items(quests, rng['Special Items'], special_item_pool)
+        replace_special_items__vanilla(quests, rng['Special Items'])
         quest_reward_ind = True
     if quest_reward_ind:
         quest_rewards = {}
