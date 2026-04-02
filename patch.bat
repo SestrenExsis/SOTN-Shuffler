3c23a0a76abf5e8f64be02b6d33780d0e2609159
SestrenExsis <sestrenexsis@gmail.com>
2026-04-01 21:57:51 -0500

    Move stage shuffling into a separate file

--- patch.bat
+++ patch.bat
@@ -1,6 +1,15 @@
+IF [%1]==[] goto :error
 
-node shuffle stage -e build/patcher/extraction-aliased.json -o build/shuffler/shuffle-stages.json
+set SEED=%1
 
-node lib/BIN-Patcher/sotn alter -s "build/patcher/extraction-masked-aliased.json" -t "build/current-patch.json"
-node lib/BIN-Patcher/sotn patch -p "build/current-patch.json" -c "build/shuffler/shuffle-stages.json"
-node lib/BIN-Patcher/sotn ppf   -p "build/current-patch.json" -t "build/current-patch.ppf"
+node shuffle stage -e build/patcher/extraction-aliased.json -o build/shuffler/shuffle-stages.json -s %SEED% || goto :error
+
+node lib/BIN-Patcher/sotn alter -s "build/patcher/extraction-masked-aliased.json" -t "build/current-patch.json" || goto :error
+node lib/BIN-Patcher/sotn patch -p "build/current-patch.json" -c "build/shuffler/shuffle-stages.json" || goto :error
+node lib/BIN-Patcher/sotn ppf   -p "build/current-patch.json" -t "build/current-patch.ppf" || goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%
+exit /b %errorlevel%
