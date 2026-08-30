0faed160f547113a138dad935738ded4fa5737c5
SestrenExsis <sestrenexsis@gmail.com>
2026-08-30 18:05:08 -0500

    Refactor setup and patch scripts

--- patch.sh
+++ patch.sh
@@ -1,31 +1,30 @@
 date
 node shuffle multi -e "build/patcher/extraction-aliased.json" -o "build/current-seed.json" \
-  --musicShuffler.on=true \
-  --patcher.on=true \
-  --patcher.list="lib/BIN-Patcher/patches/assign-power-of-wolf-relic-a-unique-id.json" \
-  --patcher.list="lib/BIN-Patcher/patches/clock-hands-display-minutes-and-seconds.json" \
-  --patcher.list="lib/BIN-Patcher/patches/customize-map-colors.json" \
-  --patcher.list="lib/BIN-Patcher/patches/enable-debug-mode.json" \
-  --patcher.list="lib/BIN-Patcher/patches/fix-boss-scylla.json" \
-  --patcher.list="lib/BIN-Patcher/patches/normalize-abandoned-mine.json" \
-  --patcher.list="lib/BIN-Patcher/patches/normalize-alchemy-laboratory.json" \
-  --patcher.list="lib/BIN-Patcher/patches/normalize-castle-entrance.json" \
-  --patcher.list="lib/BIN-Patcher/patches/normalize-clock-tower.json" \
-  --patcher.list="lib/BIN-Patcher/patches/normalize-long-library.json" \
-  --patcher.list="lib/BIN-Patcher/patches/normalize-marble-gallery.json" \
-  --patcher.list="lib/BIN-Patcher/patches/normalize-olroxs-quarters.json" \
-  --patcher.list="lib/BIN-Patcher/patches/normalize-underground-caverns.json" \
-  --patcher.list="lib/BIN-Patcher/patches/simplify-gear-puzzle.json" \
-  --rewardShuffler.on=true \
-  --roomShuffler.on=true \
-  --stageShuffler.on=true \
-  --solver.on=true \
   --hinter.seedName=true \
   --hinter.settings=true \
-  --hinter.stageLinks=true
+  --hinter.stageLinks=true \
+  --patcher.on=true \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/assign-power-of-wolf-relic-a-unique-id.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/clock-hands-display-minutes-and-seconds.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/customize-map-colors.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/enable-debug-mode.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/fix-boss-scylla.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/normalize-abandoned-mine.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/normalize-alchemy-laboratory.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/normalize-castle-entrance.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/normalize-clock-tower.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/normalize-long-library.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/normalize-marble-gallery.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/normalize-olroxs-quarters.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/normalize-underground-caverns.json" \
+  --patcher.list="lib/BIN-Patcher/bins/sotn-us/patches/simplify-gear-puzzle.json" \
+  --rewardShuffler.on=false \
+  --roomShuffler.on=true \
+  --solver.on=true \
+  --stageShuffler.on=true
 
-node lib/BIN-Patcher/sotn alter -s "build/patcher/extraction-masked-aliased.json" -t "build/current-patch.json"
-node lib/BIN-Patcher/sotn patch -p "build/current-patch.json" -c "build/current-seed.json"
-node lib/BIN-Patcher/sotn patch -p "build/current-patch.json" -c "build/patcher/change-dependencies.json"
-node lib/BIN-Patcher/sotn ppf   -p "build/current-patch.json" -t "build/current-patch.ppf"
+node lib/BIN-Patcher/bin alter -s "build/patcher/extraction-masked-aliased.json" -t "build/current-patch.json"
+node lib/BIN-Patcher/bin patch -p "build/current-patch.json" -c "build/current-seed.json"
+node lib/BIN-Patcher/bin patch -p "build/current-patch.json" -c "build/patcher/change-dependencies.json"
+node lib/BIN-Patcher/bin ppf   -p "build/current-patch.json" -t "build/current-patch.ppf"
 date
