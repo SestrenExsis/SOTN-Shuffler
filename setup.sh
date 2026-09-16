01ef077eb32e75a4261029212305b65b4dfa8400
SestrenExsis <sestrenexsis@gmail.com>
2026-09-16 05:19:07 -0500

    Convert extraction template creation from Python to NodeJS

--- setup.sh
+++ setup.sh
@@ -13,19 +13,19 @@ cp "build/patcher/aliases.json" "lib/BIN-Patcher/build/aliases.json"
 # If the next step fails, make sure you are passing the correct path to the BIN as the first argument
 # After doing so, rerun this script
 
-python3 lib/BIN-Patcher/bins/sotn-us/generate-extraction-template.py "lib/BIN-Patcher/bins/sotn-us/data/extraction-template.yaml" "build/patcher/extraction-template.json"
+node lib/BIN-Patcher/bins/sotn-us/util extract -t "lib/BIN-Patcher/bins/sotn-us/data/extraction-template.json" -o "build/patcher/extraction-template.json"
 node lib/BIN-Patcher/bin extract -b "$BIN" -t "build/patcher/extraction-template.json" -e "build/patcher/extraction.json"
 
-python3 lib/BIN-Patcher/bins/sotn-us/generate-extraction-template.py "lib/BIN-Patcher/bins/sotn-us/data/extraction-template.yaml" "build/patcher/extraction-template.json" --previous "build/patcher/extraction.json"
+node lib/BIN-Patcher/bins/sotn-us/util extract -t "lib/BIN-Patcher/bins/sotn-us/data/extraction-template.json" -o "build/patcher/extraction-template.json" --previous "build/patcher/extraction.json"
 node lib/BIN-Patcher/bin extract -b "$BIN" -t "build/patcher/extraction-template.json" -e "build/patcher/extraction.json"
 
-python3 lib/BIN-Patcher/bins/sotn-us/generate-extraction-template.py "lib/BIN-Patcher/bins/sotn-us/data/extraction-template.yaml" "build/patcher/extraction-template.json" --previous "build/patcher/extraction.json"
+node lib/BIN-Patcher/bins/sotn-us/util extract -t "lib/BIN-Patcher/bins/sotn-us/data/extraction-template.json" -o "build/patcher/extraction-template.json" --previous "build/patcher/extraction.json"
 node lib/BIN-Patcher/bin extract -b "$BIN" -t "build/patcher/extraction-template.json" -e "build/patcher/extraction.json"
 
-python3 lib/BIN-Patcher/bins/sotn-us/generate-extraction-template.py "lib/BIN-Patcher/bins/sotn-us/data/extraction-template.yaml" "build/patcher/extraction-template.json" --previous "build/patcher/extraction.json"
+node lib/BIN-Patcher/bins/sotn-us/util extract -t "lib/BIN-Patcher/bins/sotn-us/data/extraction-template.json" -o "build/patcher/extraction-template.json" --previous "build/patcher/extraction.json"
 node lib/BIN-Patcher/bin extract -b "$BIN" -t "build/patcher/extraction-template.json" -e "build/patcher/extraction.json"
 
 node lib/BIN-Patcher/bins/sotn-us/util teleporters -e "build/patcher/extraction.json" -o "build/patcher/extraction-processed.json"
-python3 lib/BIN-Patcher/bins/sotn-us/generate-change-dependencies-template.py "lib/BIN-Patcher/bins/sotn-us/data/change-dependencies-template.yaml" "build/patcher/change-dependencies.json"
+python3 lib/BIN-Patcher/bins/sotn-us/generate-change-dependencies-template.py "lib/BIN-Patcher/bins/sotn-us/data/change-dependencies-template.json" "build/patcher/change-dependencies.json"
 node lib/BIN-Patcher/bin alter -s "build/patcher/extraction-processed.json" -t "build/patcher/extraction-aliased.json" --aliases "build/patcher/aliases.json"
 node lib/BIN-Patcher/bin alter -s "build/patcher/extraction-aliased.json" -t "build/patcher/extraction-masked-aliased.json" --mask "data"
